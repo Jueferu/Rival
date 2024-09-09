48b3d8ee45bf705b55b1be255b775a6b9ab56221
José Antônio <89652772+Jueferu@users.noreply.github.com>
2024-09-09 06:33:31 -0300

    Refactor build_rocketsim_env() to update state_setter and rewards

--- main.py
+++ main.py
@@ -53,7 +53,19 @@ def build_rocketsim_env():
     from state_setters.shot_state import ShotState
     from rlgym_sim.utils.state_setters import RandomState, DefaultState
 
-    state_setter = RandomState(True, True, False)
+    state_setter = WeightedSampleSetter.from_zipped(
+        RandomState(True, True, False),
+        WallPracticeState(),
+        KickoffLikeSetter(),
+        GoaliePracticeState(),
+        DribblingStateSetter(),
+        JumpShotState(),
+        SaveState(),
+        SaveShot(),
+        SideHighRoll(),
+        ShotState(),
+        DefaultState(),
+    )
     
     from rewards.zero_sum_reward import ZeroSumReward
     from rewards.distribute_rewards import DistributeRewards
@@ -75,15 +87,30 @@ def build_rocketsim_env():
 
     from rlgym_sim.utils.reward_functions.common_rewards import EventReward
 
+    goal_reward = 1
+    agression_bias = .2
+    concede_reward = -goal_reward * (1 - agression_bias)
+    
+    team_spirit = .3
+    opp_scale = .3
+
     rewards = CombinedReward.from_zipped(
-        (EventReward(touch=1), 20),
-        (VelocityPlayerToBallReward(), 5),
-        (PlayerFaceBallReward(), 1),
-        (AirReward(), .15),
+        (ZeroSumReward(TouchBallRewardScaledByHitForce(), team_spirit, opp_scale), 5),
+        (ZeroSumReward(VelocityPlayerToBallReward(), team_spirit, opp_scale), 4),
+        (PlayerFaceBallReward(), .5),
+        (AirReward(), .1),
+        
+        (ZeroSumReward(PlayerIsClosestBallReward(), team_spirit, opp_scale), 2.5),
+        (ZeroSumReward(TouchedLastReward(), team_spirit, opp_scale), 6),
+        (VelocityBallToGoalReward(), 15),
+        (EventReward(team_goal=goal_reward, concede=concede_reward), 30),
+        (PlayerVelocityReward(), 3)
+        
+        (KickoffProximityReward(), 30),
     )
 
     spawn_opponents = True
-    team_size = 1
+    team_size = 2
     tick_skip = 8
 
     no_touch_seconds = 10
@@ -120,7 +147,7 @@ if __name__ == "__main__":
 
     n_proc = 40
     min_inference_size = max(1, int(round(n_proc * 0.9)))
-    ts_per_iteration = 50_000
+    ts_per_iteration = 100_000
 
     try:
         checkpoint_load_dir = get_most_recent_checkpoint()
@@ -138,15 +165,15 @@ if __name__ == "__main__":
                       exp_buffer_size=ts_per_iteration*4,
                       ppo_minibatch_size=25_000,
                       ppo_ent_coef=0.01,
-                      ppo_epochs=4,
+                      ppo_epochs=3,
                       standardize_returns=True,
                       standardize_obs=False,
                       save_every_ts=1_000_000,
                       policy_layer_sizes=[2048, 2048, 1024, 1024],
                       critic_layer_sizes=[2048, 2048, 1024, 1024],
                       timestep_limit=10e15,
-                      policy_lr=3e-4,
-                      critic_lr=3e-4,
+                      policy_lr=2e-4,
+                      critic_lr=2e-4,
                       render=False,
                       render_delay=8/240)
     
